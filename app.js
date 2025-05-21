59a84be66db3097323ddd2a74a2c3aad3d69d842
Jo-Writes-Code <jovingogadi01@gmail.com>
2025-05-21 21:25:54 +0300

    Javascript Intro

---
+++ app.js
@@ -0,0 +1,44 @@
+let string = "ASAP Frontend";
+
+console.log(string);
+console.log(string[0]);
+console.log(string.length);
+console.log(string[string.length - 1]);
+console.log(10 % 3);
+console.log((10 * 5) / 2);
+1 + 1;
+
+let string2 = null;
+let age = 17;
+
+const points = 100;
+
+// Comment
+age += 1;
+
+console.log(age);
+console.log(points);
+
+// Currency Conversion
+let USD = 1200;
+let AUD = USD * 1.5;
+
+console.log(USD + "$ = " + AUD + "AU$");
+
+// Equality Comparison Operator
+
+console.log('6' == 6)
+console.log('6' === 6)
+
+console.log('6' != 6)
+console.log('6' !== 6)
+
+// Conditional Statments
+let hasMembership = true
+
+if (hasMembership === true) {
+  console.log('pass')
+}
+else {
+  console.log('false')
+}
