11bd2b806477f74325731fcccc5efab2171ae4a8
Jo-Writes-Code <jovingogadi01@gmail.com>
2025-05-21 23:53:05 +0300

    loops

--- app.js
+++ app.js
@@ -27,18 +27,65 @@ console.log(USD + "$ = " + AUD + "AU$");
 
 // Equality Comparison Operator
 
-console.log('6' == 6)
-console.log('6' === 6)
+console.log("6" == 6);
+console.log("6" === 6);
 
-console.log('6' != 6)
-console.log('6' !== 6)
+console.log("6" != 6);
+console.log("6" !== 6);
 
 // Conditional Statments
-let hasMembership = true
+let hasMembership = false;
+let signedIn = true;
 
 if (hasMembership === true) {
-  console.log('pass')
+  console.log("Welcome");
+} else if (signedIn === true) {
+  console.log("Upgrade");
+} else {
+  console.log("Login");
 }
-else {
-  console.log('false')
+
+let ageRequired = 18;
+let customerAge = 16;
+let yearsLeft = ageRequired - customerAge;
+
+if (customerAge >= ageRequired) {
+  console.log(`${customerAge} years old.`);
+  console.log("Welcome!");
+} else {
+  console.log(`${customerAge} years old.`);
+  console.log(`You are ${yearsLeft} years too young.`);
 }
+
+// Logical Operators
+let registeredAge = 19;
+let hasId = true;
+
+if (registeredAge >= 18 && hasId) {
+  console.log("Enter..");
+}
+
+if (!hasId === false) {
+  console.log("Nice Id");
+}
+
+if (null) {
+  console.log(Boolean(null));
+} else if (age) {
+  console.log(Boolean(age));
+}
+
+// Ternary Operator
+let isRaining = true;
+
+isRaining ? console.log("Its raining.") : console.log("Its not raining.");
+let str = registeredAge >= 18 && hasId ? "Enter.." : "Dont Enter.";
+
+console.log(str)
+
+let userLoggedIn = false
+let userHasMembership = true
+
+let permission = (userLoggedIn && userHasMembership) ? "show video." : "dont show video.";
+
+console.log(permission)
